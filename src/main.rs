49e54b4dfa386f7897b9ccdf7dc6f8503e62524a
nyanm <nyanm2k@gmail.com>
2025-04-28 21:42:17 +0800

    complete 0003

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod r0001_two_sum;
 mod r0002_add_two_numbers;
+mod r003_longest_substring_without_repeating_characters;
 
 fn main()
 {
---
+++ src/r003_longest_substring_without_repeating_characters.rs
@@ -0,0 +1,36 @@
+use std::cmp::max;
+use std::collections::HashMap;
+
+pub struct Solution;
+impl Solution {
+    pub fn length_of_longest_substring(s: String) -> i32 {
+        let vec_s = s.chars().collect::<Vec<char>>();
+        let mut map_last_seen: HashMap<char, usize> = HashMap::new();
+
+        let mut answer: i32 = 0;
+        let mut ptr_back: i32 = -1;
+
+        for index in 0..vec_s.len()
+        {
+            let current_char = vec_s[index];
+            let last_seen = map_last_seen.get(&current_char);
+            // seen before
+            if last_seen.is_some()
+            {
+                let last_seen = *last_seen.unwrap();
+                // advance ptr_back & delete old record
+                while ptr_back < last_seen as i32
+                {
+                    ptr_back += 1;
+                    let _ = &map_last_seen.remove(&vec_s[ptr_back as usize]);
+                }
+            }
+
+            // update index info and answer
+            map_last_seen.insert(current_char, index);
+            answer = max(answer, index as i32 - ptr_back);
+        }
+
+        return answer;
+    }
+}
